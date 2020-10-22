ced7d5b4671e8790a235298db4a4a0d819e0475f
MD Alauddin <alauddinm920@gmail.com>
2020-10-22 11:30:51 +0600

    Before EmptyCart Component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom"
 import './index.css';
 import App from './App';
+import {ProductProvider} from './context';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProductProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProductProvider>
+  ,
   document.getElementById('root')
 );
 
